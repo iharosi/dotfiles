67fbb719fbe4566bfcb4e46395764bbe5cbabad2
Gergely Iharosi <iharosi@gmail.com>
2021-06-03 01:49:35 +0200

    Upgrade NodeJS to latest LTS

--- 03-nodejs.sh
+++ 03-nodejs.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env zsh
 
 # Install latest LTS version
-fnm i 12
+fnm i 14
 
 # Set latest LTS as default
-fnm default 12
+fnm default 14
 
 # Use latest LTS
-fnm use 12
+fnm use 14
 
 # Install global packages
-npm i -g diff-so-fancy eslint full-icu homebridge nodemon npm prettier tldr
+npm i -g diff-so-fancy eslint full-icu nodemon prettier tldr
